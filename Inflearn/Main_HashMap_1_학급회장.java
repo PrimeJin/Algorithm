3e367415744968aff83dcd3957845cc4e652ed63
조우진 <sky5962146@gmail.com>
2023-01-14 23:16:11 +0900

    Update Inflearn_HashMap_1_학급회장

--- Inflearn/Main_HashMap_1_학급회장.java
+++ Inflearn/Main_HashMap_1_학급회장.java
@@ -1,28 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class Main_HashMap_1_학급회장 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());  //반 학생수
         String vote = br.readLine();
-        int len=vote.length();
 
-        int[] candidate = new int[5];  //후보자 득표수 배열
-        for(int i=0; i<len; i++) {
-            candidate[vote.charAt(i)-65]++;
+        //기존 배열 사용 방법
+//        int[] candidate = new int[5];  //후보자 득표수 배열
+//        for(int i=0; i<N; i++) {
+//            candidate[vote.charAt(i)-65]++;
+//        }
+//
+//        int max=0, maxIdx=0;
+//        for(int i=0; i<5; i++) {
+//            if(max < candidate[i]) {
+//                max = candidate[i];
+//                maxIdx = i;
+//            }
+//        }
+//        System.out.println((char)(65+maxIdx));
+        
+        //HashMap 사용 방법
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char x : vote.toCharArray()) {
+        	map.put(x, map.getOrDefault(x, 0)+1);
         }
-
-        // HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        // map.put('A', ca
-        int max=0, maxIdx=0;
-        for(int i=0; i<5; i++) {
-            if(max < candidate[i]) {
-                max = candidate[i];
-                maxIdx = i;
-            }
+        int max = 0;
+        char leader = ' ';
+        for(char key : map.keySet()) {
+        	if(map.get(key) > max) {
+        		max = map.get(key);
+        		leader = key;
+        	}
         }
-        System.out.println((char)(65+maxIdx));
+        System.out.println(leader);
 	}
 }
