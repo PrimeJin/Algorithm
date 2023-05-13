d83d1a22d606cf6f968d20c3848512333282695b
조우진 <sky5962146@gmail.com>
2023-05-14 00:07:52 +0900

    Update Solution_PRO_전력망을둘로나누기

--- Programmers/Solution_PRO_전력망을둘로나누기.java
+++ Programmers/Solution_PRO_전력망을둘로나누기.java
@@ -7,27 +7,28 @@ class Solution {
         int answer = Integer.MAX_VALUE;
         connect = new boolean[n+1][n+1];
         
-        
+        //전력망 연결상태 초기화
         for(int i=0; i<wires.length; i++) {
             connect[wires[i][0]][wires[i][1]] = true;
             connect[wires[i][1]][wires[i][0]] = true;
         }
         
-        
+        //전력망 연결 하나씩 끊어보기
         for(int i=0; i<wires.length; i++) {
             connect[wires[i][0]][wires[i][1]] = false;
             connect[wires[i][1]][wires[i][0]] = false;
             
             check = new boolean[n+1];
             count = 0;
-            dfs(wires[i][0], 1, n, wires);
-            int cnt1 = count;
-            int cnt2 = n - cnt1;
+            dfs(1, 1, n, wires);
+            int cnt1 = count;  //1을 포함한 전력망 개수
+            int cnt2 = n - cnt1;  //1을 포함하지 않는 전력망 개수
             
             answer = Math.min(answer, Math.abs(cnt1-cnt2));
             
-            connect[wires[i][0]][wires[i][1]] = false;
-            connect[wires[i][1]][wires[i][0]] = false;
+            //끊어뒀던 전력망 다시 연결처리
+            connect[wires[i][0]][wires[i][1]] = true;
+            connect[wires[i][1]][wires[i][0]] = true;
         }
         
         return answer;
@@ -35,7 +36,7 @@ class Solution {
     
     public void dfs(int idx, int cnt, int n, int[][] wires) {
         check[idx] = true;
-        count = Math.max(count, cnt);
+        count++;
         
         for(int i=1; i<=n; i++) {
             if(!check[i] && connect[idx][i]) {
