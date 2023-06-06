2c32d50521ebe6e8a9caabf4597eaff6ecb59cdf
조우진 <sky5962146@gmail.com>
2023-06-07 00:12:11 +0900

    Update BOJ_1107_리모콘 주석추가

--- BaekJoon/Main_1107_리모콘.java
+++ BaekJoon/Main_1107_리모콘.java
@@ -20,11 +20,11 @@ public class Main_1107_리모콘 {
 		
 		int normalMove = Math.abs(N - 100);
 		int move = Integer.MAX_VALUE;
-		for(int i=0; i<1000000; i++) {
+		for(int i=0; i<1000000; i++) {	//브루트포스로 999999채널까지 눌러보는 경우 대비
 			String str = "" + i;
 			int len = str.length();
 			
-			boolean flag = false;
+			boolean flag = false;  //해당 채널만들때 고장난 버튼이 있는지 여부체크
 			for(int j=0; j<len; j++) {
 				if(broken[str.charAt(j) - '0']) {
 					flag = true;
@@ -32,13 +32,13 @@ public class Main_1107_리모콘 {
 				}
 			}
 			
-			if(!flag) {
-				int cnt = Math.abs(N-i) + len;
-				move = Math.min(move, cnt);
+			if(!flag) {  //고장난 버튼없이 만든 채널이면
+				int cnt = Math.abs(N-i) + len;  //Math.abs(N-i)이 채널에서 목표채널로 +or-로 이동하는데 필요한 횟수 + (len)채널입력하는데 필요한 누른 횟수
+				move = Math.min(move, cnt);  //가장 최소 횟수 저장
 			}
 		}
 		
-		System.out.print(Math.min(normalMove, move));
+		System.out.print(Math.min(normalMove, move));  //100에서 + -로 이동하는 것보다 누른 횟수가 적은지 비교
 	}
 		
 }
